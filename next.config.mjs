55ce9888b108011bc245420ba9bc2aefc2daf785
shivang <shivangagarwal3165@gmail.com>
2024-07-19 02:54:26 +0530

    nav

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+const nextConfig = {
+    images: {
+      domains: ['assets.aceternity.com'], // Add your image hostname here
+    },
+  };
+  
+  export default nextConfig;
+  
